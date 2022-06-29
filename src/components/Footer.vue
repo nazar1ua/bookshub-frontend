f6cb1a7f400997a22177903b1a7955b4a4d1a33c
Nazar Niphone <ndzobas@gmail.com>
2022-06-29 16:53:13 +0300

    Добавлено підвал

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,11 @@
 <template>
+  <footer class="bg-gray-800 mt-auto">
+    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+      <div class="mt-8 md:mt-0 md:order-1">
+        <p class="text-center text-base text-gray-300">&copy; 2020 BooksHub, Org. Всі права зареєстровано.</p>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -8,5 +15,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
